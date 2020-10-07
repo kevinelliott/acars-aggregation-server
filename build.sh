e406e6538c9449cbd94c18531fdc197359201647
Kevin Elliott <kevin@welikeinc.com>
2020-10-06 17:48:52 -0700

    Build protos

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ echo Getting dependencies
 pub get
 echo
 
+echo Building protos
+protoc --dart_out=grpc:lib/src/generated -Ilib/src/protos lib/src/protos/airframes.proto
+
 echo Building
 pub run build_runner build --delete-conflicting-outputs
 echo
